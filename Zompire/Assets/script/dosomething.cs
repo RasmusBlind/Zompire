bc4d9236e39046270bbae2762bf2a1a6dc586609
apocalypsethunderstorm <rasmusblind@live.dk>
2014-04-26 13:03:00 +0200

    the new project
    due to challenges and time limitation at new simpler project has been
    started a 2d version of the same idea as the 3d game

--- Zompire/Assets/script/dosomething.cs
+++ Zompire/Assets/script/dosomething.cs
@@ -1,11 +1,12 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class dosomething : MonoBehaviour {
 
 	void OnCollisionEnter (Collision collider){
 		if (collider.gameObject.tag == "Player"){
-			collider.rigidbody.AddForce(Vector3.up * 10000);
+
+			animation.Play("trapanimation");
 			Debug.Log("hey du der");
 			audio.Play();
 
