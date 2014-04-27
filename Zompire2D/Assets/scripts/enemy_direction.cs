6f92089247a854a026e645fb48483e4195fa036b
Simone <svinke12@student.aau.dk>
2014-04-27 16:20:20 +0200

    Enemy script
    The zombie moves towards the player now

---
+++ Zompire2D/Assets/scripts/enemy_direction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy_direction : MonoBehaviour {
+
+	public Transform target;
+	public float moveSpeed;
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.LookRotation(Vector3.forward, target.position-transform.position);
+		rigidbody2D.velocity = transform.up * moveSpeed;
+
+	}
+}
