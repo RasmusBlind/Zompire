27783778f78c3e8cecf29aa2e607d05317322137
Simone <svinke12@student.aau.dk>
2014-04-27 17:23:08 +0200

    Enemy Spawn

---
+++ Zompire2D/Assets/scripts/enemy_spawn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy_spawn : MonoBehaviour {
+
+	public Transform EnemySpawn;
+	private float Timer;
+
+	void Start() {
+		Timer = Time.time +1;
+
+	}
+
+	void Update () {
+		if (Timer < Time.time) {
+			Instantiate(EnemySpawn, transform.position, transform.rotation);
+			Timer = Time.time +1;
+		}
+	}
+}
