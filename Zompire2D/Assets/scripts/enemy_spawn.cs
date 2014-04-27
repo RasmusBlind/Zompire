aa1290c6231b03b5d809b74d75ca3444adbead6c
Rasmus Blind <rasmusblind@live.dk>
2014-04-27 22:42:29 +0200

    changed spawn script
    har lige lavet en lille endring i det script så det bliver letter at
    justere spawn time :)

--- Zompire2D/Assets/scripts/enemy_spawn.cs
+++ Zompire2D/Assets/scripts/enemy_spawn.cs
@@ -5,16 +5,17 @@ public class enemy_spawn : MonoBehaviour {
 
 	public Transform EnemySpawn;
 	private float Timer;
+	public float nextspawn = 1.0f;
 
 	void Start() {
-		Timer = Time.time +1;
+		Timer = Time.time + nextspawn;
 
 	}
 
 	void Update () {
 		if (Timer < Time.time) {
 			Instantiate(EnemySpawn, transform.position, transform.rotation);
-			Timer = Time.time +1;
+			Timer = Time.time + nextspawn;
 		}
 	}
 }
