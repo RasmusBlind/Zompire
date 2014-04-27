d2e04f6f172f949bd9a5ab5a64c34b316c6d2e2f
Rasmus Blind <rasmusblind@live.dk>
2014-04-27 23:05:24 +0200

    spawn enemy script
    har lavet så hvis man sætter spawn i midten af mappet kan zombierne
    spawne over hele mappet

--- Zompire2D/Assets/scripts/enemy_spawn.cs
+++ Zompire2D/Assets/scripts/enemy_spawn.cs
@@ -6,6 +6,8 @@ public class enemy_spawn : MonoBehaviour {
 	public Transform EnemySpawn;
 	private float Timer;
 	public float nextspawn = 1.0f;
+	private float randomrangey;
+	private float randomrangex;
 
 	void Start() {
 		Timer = Time.time + nextspawn;
@@ -14,7 +16,10 @@ public class enemy_spawn : MonoBehaviour {
 
 	void Update () {
 		if (Timer < Time.time) {
-			Instantiate(EnemySpawn, transform.position, transform.rotation);
+			randomrangey = Random.Range(-20.0f , 20.0f);
+			randomrangex = Random.Range(-4.5f , 5.0f);
+			Vector2 spawnposition = new Vector2(transform.position.x + randomrangex , transform.position.y + randomrangey);
+			Instantiate(EnemySpawn, spawnposition, transform.rotation);
 			Timer = Time.time + nextspawn;
 		}
 	}
