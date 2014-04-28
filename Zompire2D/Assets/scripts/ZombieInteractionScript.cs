69af339caf7aec14e7dea7eb85d6794e786530dd
samnilsson <info@medialogy.biz>
2014-04-28 15:24:09 +0200

    Blood Splash Dissolve
    Beta disolve graphics

--- Zompire2D/Assets/scripts/ZombieInteractionScript.cs
+++ Zompire2D/Assets/scripts/ZombieInteractionScript.cs
@@ -1,14 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//The Zombie Interaction Script is added to the "enemy", so when enemy is hit by the bullet, enemy is destroyed automatically.
+
 public class ZombieInteractionScript : MonoBehaviour {
+
+	public Transform BloodSplash;
+	//public Transform SkullInTheSand;
 
 	void OnCollisionEnter2D(Collision2D Hit){
 		if (Hit.gameObject.tag == "Bullet"){
 			ScoreController.score++;
 
 			Destroy (gameObject);
-
+			Instantiate(BloodSplash,transform.position,Quaternion.identity);
+			//Instantiate(SkullInTheSand,transform.position,Quaternion.identity);
 		}
 	}
 }
