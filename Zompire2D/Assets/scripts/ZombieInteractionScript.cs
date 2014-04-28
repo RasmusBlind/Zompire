61e1b63efe9ef2403c2429704f54f712eb141f60
samnilsson <info@medialogy.biz>
2014-04-28 11:38:52 +0200

    Comment added
    Zombie interaction script documentation

--- Zompire2D/Assets/scripts/ZombieInteractionScript.cs
+++ Zompire2D/Assets/scripts/ZombieInteractionScript.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//The Zombie Interaction Script is added to the "enemy", so when enemy is hit by the bullet, enemy is destroyed automatically.
+
 public class ZombieInteractionScript : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D Hit){
