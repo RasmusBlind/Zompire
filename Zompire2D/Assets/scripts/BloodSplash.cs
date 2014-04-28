4b607238af108c6299c907aa1374c78b6cadb96b
samnilsson <info@medialogy.biz>
2014-04-28 16:23:32 +0200

    Uncomitted
    Due to GitHub error message

--- Zompire2D/Assets/scripts/BloodSplash.cs
+++ Zompire2D/Assets/scripts/BloodSplash.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class BloodSplash : MonoBehaviour {
 
-	private float Seconds= 1.0f;
+	//private float Seconds= 1.0f;
 	void Start() {
 		Destroy(gameObject,5);
 	}
