ba16c4151f4028d7958dc0ac8f87c675792b3c30
Rasmus Blind <rasmusblind@live.dk>
2014-04-28 23:39:45 +0200

    start script

--- Zompire2D/Assets/scripts/WallMovement.cs
+++ Zompire2D/Assets/scripts/WallMovement.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class WallMovement : MonoBehaviour {
 
 
-	private Transform player; //a public transform variable so it can be accesd in unity, and we can drag/drop the player as the player
+
 	private Vector2 newPosition; //variable that stores the new position x and y
 	private float xpos;
 	private float ypos;
@@ -13,16 +13,18 @@ public class WallMovement : MonoBehaviour {
 
 		xpos = transform.position.x;
 		ypos = transform.position.y;
+
 	}
 
 	void Update() {
 		Vector2 positionA =	transform.position; //position A is the first position (from A) - it is placed at (0, 2) on the level
 		Vector2 positionB = new Vector2(xpos - 11, ypos); //position B is the end position (to B) - it will be placed at (-11, 2) on the level
-		player = GameObject.FindGameObjectWithTag("Player").transform;
-		if(player.transform.position.y < transform.position.y) { //if the player's position is below the y value of 2 (which is the y value of the walls y coordinate in both position A and B) ...
+
+
+		if(simple_movement.playerposy < transform.position.y) { //if the player's position is below the y value of 2 (which is the y value of the walls y coordinate in both position A and B) ...
 			newPosition = positionA; //the position will be at position A (0,2)
 		}
-		if(player.transform.position.y >= transform.position.y ) { //if the players position is equal to or above the y value 2 - so if the player has passed this place on the level ...
+		if(simple_movement.playerposy >= transform.position.y ) { //if the players position is equal to or above the y value 2 - so if the player has passed this place on the level ...
 			newPosition = positionB; // the new position of the wall will be equal to positionB
 			audio.Play();
 
--- Zompire2D/Assets/scripts/followplayer.cs
+++ Zompire2D/Assets/scripts/followplayer.cs
@@ -1,15 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class followplayer : MonoBehaviour {
+	public class followplayer : MonoBehaviour {
 	private Vector3 mypos;
-	public Transform Player;
+
 
 	// Update is called once per frame
 	void Update () {
-		if (Player){
-			mypos = new Vector3 (0,0,-20);
-			transform.position = Player.position +  mypos;
-		}
+
+		mypos = new Vector3 (-15,simple_movement.playerposy,-20);
+		transform.position = mypos;
 	}
 }
