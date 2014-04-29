22e8f8857e19c889491893ea7073f681096a720e
Rasmus Blind <rasmusblind@live.dk>
2014-04-29 11:46:13 +0200

    small change

---
+++ Zompire2D/Assets/removeifkeyfoundis.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class removeifkeyfoundis : MonoBehaviour {
+	
+
+	// Update is called once per frame
+	void Update () {
+		if(simple_movement.gotkey == true){
+
+			Destroy (gameObject);
+		}
+	}
+}
