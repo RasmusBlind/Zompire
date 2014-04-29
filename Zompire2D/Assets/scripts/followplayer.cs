2598f957447790edb5a1ee380973cc02bf50ef29
Rasmus Blind <rasmusblind@live.dk>
2014-04-29 12:10:55 +0200

    active and deactive audiolistener

--- Zompire2D/Assets/scripts/followplayer.cs
+++ Zompire2D/Assets/scripts/followplayer.cs
@@ -3,21 +3,28 @@ using System.Collections;
 
 	public class followplayer : MonoBehaviour {
 	private Vector3 mypos;
+	public AudioListener myaudiooncam;
+
 
 
 	// Update is called once per frame
 	void Update () {
 		if (Gamemanagement.gameisalive == true){
-			mypos = new Vector3 (-15,simple_movement.playerposy,-20);
+			mypos = new Vector3 (-15,simple_movement.playerposy,-1);
 			transform.position = mypos;
 		}
 		if ( Gamemanagement.gameisalive == false && Gamemanagement.firsttime == true){
-			mypos = new Vector3 (15,-33.79374f,-20);
+			mypos = new Vector3 (15,-33.79374f,-1);
 			transform.position = mypos;
 		}
 		if ( Gamemanagement.gameisalive == false && Gamemanagement.firsttime == false){
-			mypos = new Vector3 (15,-10,-20);
+			mypos = new Vector3 (15,-10,-1);
 			transform.position = mypos;
+		}
+		if (Gamemanagement.gameisalive == true){
+			myaudiooncam.enabled = false;
+		} else {
+			myaudiooncam.enabled = true;
 		}
 	}
 }
